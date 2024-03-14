bcf3168e9fb461c23687d46437f509e5c6b57f56
DESKTOP-L8PQMEN\User <power93266@naver.com>
2024-03-14 22:28:43 +0900

    [Clean Architecture] Application API 프로젝트 제작 #4
    feat : Project 준비 완료
    
    1. 기존 확장 클래스로 작성했던 IServiceCollection 에 Service Add
    2. appsetting.json 에 기본 세팅 값 입력 완료

--- HR.LeaveManagement.Clean/HR.LeaveManagement.Api/Program.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Api/Program.cs
@@ -1,8 +1,22 @@
+using HR.LeaveManagement.Application;
+using HR.LeaveManagement.Infrastructure;
+using HR.LeaveManagement.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddPersistenceService(builder.Configuration);
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("all",builder => builder.AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
