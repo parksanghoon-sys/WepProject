a216a75d46b2f22126ffc7dcf131976908b1bc05
DESKTOP-L8PQMEN\User <power93266@naver.com>
2024-03-25 23:11:45 +0900

    [Clean Architecture] Application API 프로젝트 제작 #4
    feat : 에러 로그관련 MiddelWare 제작
    
    1. 에러로그 발생시 파이프 라인으로 연결해서 Log 기록하는 MiddleWare 클래스 생성

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Api/MiddleWare/ExceptionMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using HR.LeaveManagement.Api.Models;
+using HR.LeaveManagement.Application.Exceptions;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace HR.LeaveManagement.Api.MiddleWare
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _requestDelegate(httpContext);
+            }
+            catch (Exception ex)
+            {
+
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            CustomProblemDetails problem = new CustomProblemDetails();
+
+            switch (ex)
+            {
+                case BadRequestException badHttpRequestException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    problem = new CustomProblemDetails
+                    {
+                        Title = badHttpRequestException.Message,
+                        Status = (int)statusCode,
+                        Detail = badHttpRequestException.InnerException?.Message,
+                        Type = nameof(BadRequestException),
+                        Errors = badHttpRequestException.ValidationErrors
+                    };
+                    break;
+                case NotFoundException NotFound:
+                    statusCode = HttpStatusCode.NotFound;
+                    problem = new CustomProblemDetails
+                    {
+                        Title = NotFound.Message,
+                        Status = (int)statusCode,
+                        Type = nameof(NotFoundException),
+                        Detail = NotFound.InnerException?.Message,
+                    };
+                    break;
+                default:
+                    problem = new CustomProblemDetails
+                    {
+                        Title = ex.Message,
+                        Status = (int)statusCode,
+                        Type = nameof(HttpStatusCode.InternalServerError),
+                        Detail = ex.StackTrace,
+                    };
+                    break;
+            }
+            httpContext.Response.StatusCode = (int)statusCode;
+            var logMessage = JsonConvert.SerializeObject(problem);
+            _logger.LogError(logMessage);
+            await httpContext.Response.WriteAsJsonAsync(problem);
+        }
+    }
+}
