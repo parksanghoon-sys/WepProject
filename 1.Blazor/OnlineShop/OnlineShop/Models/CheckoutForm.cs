04ddb1fa1830409c44d1b4aa050fd3ffe03de813
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2024-07-30 15:11:57 +0900

    update : https://blazorschool.com/tutorial/blazor-server/dotnet6/component-lifecycle-431616

---
+++ 1.Blazor/OnlineShop/OnlineShop/Models/CheckoutForm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class CheckoutForm
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "We need the address to deliver to product")]
+        public string Address { get; set; } = string.Empty;
+    }
+}
