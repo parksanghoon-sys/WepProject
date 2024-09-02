3cff6c5b2b2145081a62f3d94b2e60b14956390e
DESKTOP-L8PQMEN\User <power93266@naver.com>
2024-09-02 23:31:40 +0900

    feat : 알람 Nuget 설치로 구현완료

--- HR.LeaveManagement.Clean/HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Create.razor.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Create.razor.cs
@@ -1,7 +1,9 @@
-﻿using HR.LeaveManagement.BlazorUI.Contracts;
+﻿using Blazored.Toast.Services;
+using HR.LeaveManagement.BlazorUI.Contracts;
 using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
 
 using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
 
 namespace HR.LeaveManagement.BlazorUI.Pages.LeaveTypes
 {
@@ -10,19 +12,25 @@ namespace HR.LeaveManagement.BlazorUI.Pages.LeaveTypes
         [Inject]
         NavigationManager _navManager { get; set; }
         [Inject]
-        ILeaveTypeService _client { get; set; }
+        ILeaveTypeService _client { get; set; }        
+        
         [Inject]
+        IToastService _toastService { get; set; }
         public string Message { get; private set; }
-
         LeaveTypeVM leaveType = new LeaveTypeVM();
         async Task CreateLeaveType()
         {
             var response = await _client.CreateLeaveType(leaveType);
             if (response.IsSuccess)
-            {                
+            {
+                _toastService.ShowSuccess("Leave Type created successed");
                 _navManager.NavigateTo("/leavetypes/");
             }
             Message = response.Message;
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            Debug.Write("TEST");
         }
     }
 }
