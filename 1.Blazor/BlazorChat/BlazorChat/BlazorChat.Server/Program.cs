24f18d79ef8e9b80c4e7d13fc4978c07c8d82b66
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2024-09-23 13:51:43 +0900

    fix : 시작페이지 수정
    1. 오류발생

--- 1.Blazor/BlazorChat/BlazorChat/BlazorChat.Server/Program.cs
+++ 1.Blazor/BlazorChat/BlazorChat/BlazorChat.Server/Program.cs
@@ -1,17 +1,62 @@
+using BlazorChat.Server.Data;
+using BlazorChat.Shared;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore; // UseMigrationsEndPoint 관련
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentityServer()
+    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+builder.Services.AddAuthentication()
+    .AddIdentityServerJwt();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts(); // Default HSTS value is 30 days. Change for production if necessary.
+}
 
 app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles(); // Blazor WebAssembly 파일 제공
+app.UseStaticFiles();
 
+app.UseRouting();
+
+app.UseIdentityServer();
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
+app.MapHub<SignalRHub>("/signalRHub");
 
 app.Run();
---
+++ 1.Blazor/BlazorChat/BlazorChat/BlazorChat.Server/SignalRHub.cs
@@ -0,0 +1,14 @@
+﻿using BlazorChat.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+public class SignalRHub : Hub
+{
+    public async Task SendMessageAsync(ChatMessage message, string userName)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message, userName);
+    }
+    public async Task ChatNotificationAsync(string message, string receiverUserId, string senderUserId)
+    {
+        await Clients.All.SendAsync("ReceiveChatNotification", message, receiverUserId, senderUserId);
+    }
+}
